8a3f79a5f2703b87e08c680d9fc5c0e59e6ac381
Tanguy Lefort <tanguy_l@outlook.fr>
2020-06-18 11:31:21 +0200

    basis for cg with revcom
    Check what's going on with the re-normalisation of the errors, and what to do with the preconditioner

--- pykeops/torch/cg.py
+++ pykeops/torch/cg.py
@@ -13,13 +13,17 @@ from pykeops.common.utils import get_tools
 
 ##################  Main routines
 
-def cg(A, b, binding, x=None, M=None,  eps=None, maxiter=None, regul=None, inv_precond=None):
+def cg(linop, b, binding, x=None, M=None,  eps=None, maxiter=None, regul=None, inv_precond=None):
     if binding not in ("torch", "numpy", "pytorch"):
         raise ValueError("Language not supported, please use numpy, torch or pytorch.")
     
     tools = get_tools(binding)
-    A, b, x, M, replaced = check_dims(A, b, x, M, tools)
-    n = A.shape[0]
+    ################################
+    ##### Not as easy with linop
+    ###############################
+    b, x, M, replaced = check_dims(b, x, M, tools)
+    x = x.reshape(-1)
+    n = b.shape[0]
 
     if eps == None:
         eps = 1e-10
@@ -36,7 +40,7 @@ def cg(A, b, binding, x=None, M=None,  eps=None, maxiter=None, regul=None, inv_p
     # third are the q_i size n
     # fourth(added in the else of M is None) is the z_i size n
 
-    data_vect = tools.zeros(4 * n, dtype=A.dtype)
+    data_vect = tools.zeros(4 * n, dtype=b.dtype)
     scal1, scal2 = None, None # init the scala values
 
     # cg subroutines will have a While not max iter reached
@@ -44,24 +48,27 @@ def cg(A, b, binding, x=None, M=None,  eps=None, maxiter=None, regul=None, inv_p
     job, step = 1, 1
 
     while iter_ <= maxiter:
-        job, step, data_vect, iter_, scal1 ,scal2 = revcom(M, b, job, step, data_vect, iter_, n, eps, scal1, scal2)
+        print("cg", job, step, iter_)
+        job, step, x, data_vect, iter_, scal1 ,scal2 = revcom(M, b, x, job, step, data_vect, iter_, n, eps, scal1, scal2)
 
         if job == 1: # matrix by vector product
-            print("something")
+            data_vect[2*n:3*n] = linop(data_vect[n:2*n].reshape(-1, 1))
+            job, step = 2, 3
 
         elif job == 2: # precond solver, multiple steps ?
-            #compute the zi
+            data_vect[3*n:4*n] = inv_precond(M, data_vect[0:n])
             job = 2
 
         elif job == 3: # matrix by x product
             if iter_ == 0 and not replaced:
-                data_vect[0:n] = A @ x
+                data_vect[0:n] = linop(x.reshape(-1, 1))
             job, step = 1, 2
 
         elif job == 4: # check norm errors
-            rando = tools.rand() 
-            data_vect[0:n] = b - A @ data_vect[0:n] if rando <= 0.1 else data_vect[0:n] # simple stocha condi regul residuals
-            job, resid = should_stop(data_vect[0:n], A, b, eps, n)
+            rando = tools.rand(1, 1)
+            print("random", rando <= 0.1)
+            data_vect[0:n] = b.reshape(-1) - linop(data_vect[0:n].reshape(-1)) if rando <= 0.1 else data_vect[0:n] # simple stocha condi regul residuals
+            job, resid = should_stop(data_vect[0:n], eps, n)
             if job == -1:
                 break;
             else:
@@ -70,27 +77,33 @@ def cg(A, b, binding, x=None, M=None,  eps=None, maxiter=None, regul=None, inv_p
 
     return x, resid, iter_
 
-def should_stop(data, A, b, eps, n):
+def should_stop(data, eps, n):
     nrmresid2 = data[0:n].T @ data[0:n]
     njob = -1 if nrmresid2 <= eps**2 else 2 # the cycle restarts at the precond solver step
+    print("stop", njob)
     return njob, nrmresid2
 
-def revcom(M, b, job, step, data, iter_, n, eps, scal1, scal2):
+def revcom(M, b, x, job, step, data, iter_, n, eps, scal1, scal2):
+    print("revcom", job, step, iter_)
     if job == 1: # init step
         if step == 1:
             njob = 3
         else:
-            data[0:n] = b - data[0:n]
+            data[0:n] = b.reshape(-1) - data[0:n]
             njob = 4
 
     elif job == 2: # resume
-        if step == 1 and M is not None: # we need the zi 
-            njob = 2
+        if step == 1:
+            if M is not None: # we need the zi 
+                njob = 2
             step += 1
-        elif step == 2:
+
+        if step == 2:
+            scal2 = scal1
             if M is None:
                 scal1 = data[0:n].T @ data[0:n]
                 if iter_ > 1:
+                    print(scal1, scal2)
                     beta = scal1 / scal2 #rho i-1 / rho i-2
                     data[n:2*n] = data[0:n] + beta * data[n:2*n] # r i-1 + beta i-1 p i-1
                 else:
@@ -102,9 +115,14 @@ def revcom(M, b, job, step, data, iter_, n, eps, scal1, scal2):
                     data[n:2*n] = data[3*n:4*n] + beta * data[n:2*n] # r i-1 + beta i-1 p i-1
                 else:
                     data[n:2*n] = data[3*n:4*n] # p1=z0
-                    
-
-    return njob, step, data, iter_, scal1, scal2
+            njob = 1
+        elif step == 3:
+            alpha = scal1 / (data[n:2*n].T @ data[2*n:3*n])
+            x += alpha * data[n:2*n]
+            data[0:n] -= alpha * data[2*n:3*n]
+            njob = 4
+    print("revcomfin", njob, step, iter_)
+    return njob, step, x, data, iter_, scal1, scal2
 
 
 
@@ -144,47 +162,59 @@ def switcher_revcom(job, M):
 
 ############### SafeGuard routines
 
-def check_dims(A, b, x, M, tools):
-    nrow = A.shape[0]
+def check_dims(b, x, M, tools): # The actual kernel can't be used for the sizes (we only know the linop)
+    nrow = b.shape[0]
     x_replaced = False
 
-    if nrow != A.shape[1]: # check symmetry
-        raise ValueError("The matrix is expected to be squared, not of shape {}.".format(A.shape))
-
     if x is None: # check x shape and initiate it if needed
-        x = tools.zeros((nrow, 1), dtype=A.dtype)
+        x = tools.zeros((nrow, 1), dtype=b.dtype)
         x_replaced = True
     elif (nrow, 1) != x.shape:
             if x.shape == (nrow,):
                 x = x.reshape((nrow, 1)) # just recast it
             else:
-                raise ValueError("Mismatch between shapes of A {} and shape of x {}.".format(A.shape, x.shape))
+                raise ValueError("Mismatch between shapes of the kernel {} and shape of x {}.".format((nrow, nrow), x.shape))
 
     if x.shape != b.shape: # check RHS shape
         if b.shape == (nrow,):
             b = b.reshape((nrow, 1)) # not necessary to throw an error for that, just reshape
         else:
-            raise ValueError("Mismatch between shapes of x {} and shape of b {}.".format(A.shape, b.shape))
+            raise ValueError("Mismatch between shapes of x {} and shape of b {}.".format(x.shape, b.shape))
 
-    if M is not None: # check preconditioner dims
-        if M.shape != A.shape:
-            raise ValueError("Mismatch between shapes of M {} and shape of A {}.".format(M.shape, A.shape))
-    return A, b, x, M, x_replaced # A was never changed
+    if M is not None: # check preconditioner dimsfrom pykeops.common.utils import get_tools
+
+        if M.shape != (nrow, nrow):
+            raise ValueError("Mismatch between shapes of M {} and shape of the kernel {}.".format(M.shape,(nrow, nrow)))
+    return b, x, M, x_replaced 
 
 
 
 ############ test
 import torch
-from pykeops.torch import Genred
+import numpy as np
+#from pykeops.torch import Genred
+from pykeops.numpy import Genred
+import pykeops
 
-size = 10
+
+size = 5
 
 formula = "Exp(-SqDist(x, y)) * a"  # Exponential kernel
 aliases =  ["x = Vi(1)",  # 1st input: target points, one dim-3 vector per line
             "y = Vj(1)",  # 2nd input : dim-3 per columns
             "a = Vj(1)"]
 
-xi = torch.linspace(1/size, 1, size)
-b = torch.rand(size, 1)
+#xi = torch.linspace(1/size, 1, size).view(-1,1)from pykeops.common.utils import get_tools
+
+#b = torch.rand(size, 1).view(-1,1)
 K = Genred(formula, aliases, axis = 1)
-print(K)
+xnum = np.linspace(1/size, 1, size).reshape(-1,1)
+bnum = np.random.rand(size, 1)
+
+def linop(vect):
+    global xnum
+    vect = vect.reshape(-1,1)
+    return K(xnum, xnum, vect).reshape(-1)
+pykeops.verbose = True
+print(linop(bnum))
+print(cg(linop, bnum, "numpy"))
