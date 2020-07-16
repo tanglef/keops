b929e0b7eac2d2183a402cdb942434477251f94d
Tanguy Lefort <tanguy_l@outlook.fr>
2020-07-16 18:45:00 +0200

    working doc page

--- pykeops/benchmarks/plot_benchmark_cg.py
+++ pykeops/benchmarks/plot_benchmark_cg.py
@@ -5,12 +5,14 @@ Conjugate gradient method
 Different implementations of the conjugate gradient (CG) exist. Here, we compare the CG implemented in scipy which uses Fortran
 against it's pythonized version and the older version of the algorithm available in pykeops.
 
-We want to solve the positive definite linear system :math:`(K_{x,x} + \alpha Id)a = b` for :math:`a, b, x \in \mathbb R^N`.
+We want to solve the positive definite linear system :math:`(K_{x,x} + \\alpha Id)a = b` for :math:`a, b, x \in \mathbb R^N`.
 
 Let the Gaussian RBF kernel be defined as
+
 .. math::
 
-    K_{x,x}=\left[\exp(-\gamma \|x_i - x_j\|^2)\right]_{i,j=1}^N. 
+    K_{x,x}=\left[\exp(-\gamma \|x_i - x_j\|^2)\\right]_{i,j=1}^N. 
+
 
 Choosing :math:`x` such that :math:`x_i = i/N,\ i=1,\dots, N` makes :math:`K_{x,x}` be a highly unwell-conditioned matrix for :math:`N\geq 10`.
 
@@ -46,7 +48,7 @@ device = torch.device("cuda") if use_cuda else torch.device("cpu")
 print("The device used is", device, '.')
 
 ########################################
-# gaussian radial basis function kernel
+# Gaussian radial basis function kernel
 ########################################
 
 formula = 'Exp(- g * SqDist(x,y)) * a' # linear w.r.t a
@@ -59,11 +61,12 @@ aliases = ['x = Vi(1)',   # First arg:  i-variable of size 1
 ############################
 # Functions to benchmark
 ###########################
-
+#
 # All systems are regularized with a ridge parameter ``alpha``. 
-
+#
 # The originals :
 # -------------------
+
 
 def keops_tch(x, b, gamma, alpha, callback=None):
     Kinv = KernelSolve(formula, aliases, "a", axis=1, dtype='float32')
@@ -76,8 +79,11 @@ def keops_np(x, b, gamma, alpha, callback=None):
     res = Kinv(x, x, b, gamma, alpha=alpha, callback=callback)
     return res
 
+
+####################################
 # Scipy :
 # ----------
+#
 
 
 def scipy_cg(x, b, gamma, alpha, callback=None):
@@ -88,20 +94,23 @@ def scipy_cg(x, b, gamma, alpha, callback=None):
     res = cg(A, b, callback=callback)
     return res
 
+
+####################################""
 # Pythonized scipy :
 # ---------------------
+#
 
 
 def dic_cg_np(x, b, gamma, alpha, callback=None, check_cond=False):
     Kinv = KernelSolve_np(formula, aliases, "a", axis=1, dtype='float32')
-    ans = Kinv.dic_cg(x, x, b, gamma, alpha=alpha,
+    ans = Kinv.cg(x, x, b, gamma, alpha=alpha,
                       callback=callback, check_cond=check_cond)
     return ans
 
 
 def dic_cg_tch(x, b, gamma, alpha, check_cond=False):
     Kinv = KernelSolve(formula, aliases, "a", axis=1, dtype='float32')
-    ans = Kinv.dic_cg(x, x, b, gamma, alpha=alpha, check_cond=check_cond)
+    ans = Kinv.cg(x, x, b, gamma, alpha=alpha, check_cond=check_cond)
     return ans
 
 
@@ -118,10 +127,14 @@ reps = [100,  100, 50,  10,   10,   5,     5]
 
 
 def compute_error(func, pack, result, errors, x, b, alpha, gamma):
-
-    code = "a = func(x, b, gamma, alpha)[0].reshape(b.shape);\
-            err = ( (alpha * a + K(x, x, a, gamma) - b) ** 2).sum();\
-            errors.append(err);"
+    if str(func)[10:15] == "keops":
+        code = "a = func(x, b, gamma, alpha).reshape(b.shape);\
+                err = ( (alpha * a + K(x, x, a, gamma) - b) ** 2).sum();\
+                errors.append(err);"
+    else:
+        code = "a = func(x, b, gamma, alpha)[0].reshape(b.shape);\
+                err = ( (alpha * a + K(x, x, a, gamma) - b) ** 2).sum();\
+                errors.append(err);"
 
     if pack == 'numpy':
         K = Genred_np(formula, aliases, axis=1, dtype='float32')
@@ -198,8 +211,11 @@ def global_bench(functions, sizes, reps):
 
     return list_times, list_errors
 
+
+##############################################
 # Stability
 # ------------
+#
 # Stability of the errors and norm of the iterated approximations of the answer
 
 
@@ -255,8 +271,10 @@ def norm_stability(size, funcpack):
     return errk_scipy, iter_scipy, normx_scipy, errk_dic, iter_dic, normx_keops, errk_keops, iter_keops, normx_dic
 
 
+#######################################################
 # Is the condition number too big ? 
 # -------------------------------------
+#
 # The argument ``check_cond`` lets the user have an idea of the conditioning number of the matrix :math:`A=(K_{x,x} + \alpha Id)`. A warning appears
 # if :math:`\mathrm{cond}(A)>500`. The user is also warned if the CG algorithm reached its maximum number of iterations *ie* did not converge.
 
@@ -307,8 +325,8 @@ errk_scipy, iter_scipy, normx_scipy, errk_dic, iter_dic,\
 labels = ["scipy + keops", "keops_np", "keops_tch",
           "dico + keops_np", "dico + keops_tch"]
 plt.style.use('ggplot')
-plt.figure(figsize=(20, 7))
-plt.subplot(221)
+plt.figure(figsize=(10,20))
+plt.subplot(411)
 for i in range(len(functions)):
     plt.plot(sizes, list_times[i], label=labels[i])
 plt.xscale('log')
@@ -316,7 +334,7 @@ plt.yscale('log')
 plt.xlabel(r"Kernel of size $n\times n$")
 plt.ylabel("Computational time (s)")
 plt.legend()
-plt.subplot(222)
+plt.subplot(412)
 for i in range(len(functions)):
     plt.plot(sizes, list_errors[i], label=labels[i])
 plt.xscale('log')
@@ -325,7 +343,7 @@ plt.xlabel(r"Kernel of size $n\times n$")
 plt.ylabel(r"Error $||Ax_{k_{end}} -b||^2$")
 plt.legend()
 
-plt.subplot(223)
+plt.subplot(413)
 plt.plot(iter_keops, errk_keops, 'o-', label=labels[1])
 plt.plot(iter_scipy, errk_scipy, '^-', label=labels[0])
 plt.plot(iter_dic, errk_dic, 'x-', label=labels[3])
@@ -334,7 +352,7 @@ plt.xlabel(r"Iteration k")
 plt.ylabel(r"Iterates for the error $||r_k||^2$")
 plt.legend()
 
-plt.subplot(224)
+plt.subplot(414)
 plt.plot(iter_keops, normx_keops, 'o-', label=labels[1])
 plt.plot(iter_scipy, normx_scipy, '^-', label=labels[0])
 plt.plot(iter_dic, normx_dic, 'x-', label=labels[3])
